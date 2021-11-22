3dc217ebdab77695b482aefbabac0fcb8e91f280
Jake <jake.fay9673@gmail.com>
2021-11-21 20:33:55 -0500

    product routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -47,8 +47,6 @@ router.get('/:id', (req, res) => {
       console.log(err);
       res.status(500).json(err);
   });
-  
-  // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
